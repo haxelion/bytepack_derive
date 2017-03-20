beebef10915e0dc94dca3ab37b74ba02744caa74
Charles Hubain <github@haxelion.eu>
2017-03-20 22:20:17 +0100

    First working version of the derive, not handling generics yet

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,14 @@ extern crate syn;
 #[macro_use]
 extern crate quote;
 
+use std::collections::HashSet;
+
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(Parsed)]
+#[proc_macro_derive(Packed)]
 pub fn derive_parsed(input: TokenStream) -> TokenStream {
     let s = input.to_string();
-    let ast = syn::parse_macro_input(&s).unwrap();
+    let ast = syn::parse_derive_input(&s).unwrap();
 
     match impl_parsed(&ast) {
     	Ok(gen) => gen.parse().unwrap(),
@@ -16,11 +18,23 @@ pub fn derive_parsed(input: TokenStream) -> TokenStream {
 	}
 }
 
-fn impl_parsed(ast: &syn::MacroInput) -> Result<quote::Tokens, String> {
+fn impl_parsed(ast: &syn::DeriveInput) -> Result<quote::Tokens, String> {
     let name = &ast.ident;
+    let mut type_set = HashSet::<syn::Ty>::new();
+    let mut members = Vec::<syn::Ident>::new();
+    if let syn::Body::Enum(_) = ast.body {
+        return Err("Cannot implement Parsed for an enum".to_string());
+    }
+    if let syn::Body::Struct(syn::VariantData::Struct(ref fields)) = ast.body {
+        for ref field in fields {
+            type_set.insert(field.ty.clone());
+            members.push(field.ident.clone().unwrap());
+        }
+    }
     Ok(quote! {
-        impl Parsed for #name {
-            fn endianness_switch(&mut self) {
+        impl Packed for #name where #( #type_set : Packed ),* {
+            fn switch_endianness(&mut self) {
+                #( self.#members.switch_endianness(); )*
             }
         }
     })
